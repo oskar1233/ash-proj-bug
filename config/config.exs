4d1bcaea83d128670eca5c3c023245a9052bfe81
Oskar Laska <oskar.laska@surferseo.com>
2024-07-16 15:24:05 +0200

    Fresh Ash project

---
+++ config/config.exs
@@ -0,0 +1,25 @@
+import Config
+
+config :spark,
+  formatter: [
+    remove_parens?: true,
+    "Ash.Resource": [
+      section_order: [
+        :resource,
+        :code_interface,
+        :actions,
+        :policies,
+        :pub_sub,
+        :preparations,
+        :changes,
+        :validations,
+        :multitenancy,
+        :attributes,
+        :relationships,
+        :calculations,
+        :aggregates,
+        :identities
+      ]
+    ],
+    "Ash.Domain": [section_order: [:resources, :policies, :authorization, :domain, :execution]]
+  ]
